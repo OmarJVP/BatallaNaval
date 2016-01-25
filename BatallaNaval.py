e9b48269ad7e41a77a28ad711d38d78a46e22b24
Omar <omi.jvp@gmail.com>
2016-01-25 15:14:17 -0600

    Mejoras para el usuario, ya no tiene que poner el 0, su ingreso de coordenadas es del 1-5

--- BatallaNaval.py
+++ BatallaNaval.py
@@ -13,37 +13,41 @@ def print_tablero(tablero):
 
 #FUNCIONES PARA DEFINIR LA POSICION DEL BARCO DE FORMA ALEATORIA
 def fila_aleatoria(tablero):
-    return randint(0, len(tablero)-1)
+    return randint(1, len(tablero))
 
 def columna_aleatoria(tablero):
-    return randint(0, len(tablero)-1)
+    return randint(1, len(tablero))
 
 print "Juguemos batalla naval"
 print "INTENTA HUNDIR MI BARCO"
 print_tablero(tablero)
 
-barco_fila = fila_aleatoria(tablero)
-barco_col = columna_aleatoria(tablero)
+barco_fila = fila_aleatoria(tablero) 
+barco_col = columna_aleatoria(tablero) 
 
 print barco_fila
 print barco_col
 
-for turno in range(5):
-	print "TURNO: " + str(turno+1)
-	adivina_fila = int(raw_input("Adivina fila: "))
+for turno in range(1,5):
+	print 
+	print "TURNO: " + str(turno)
+	adivina_fila = int(raw_input("Adivina fila: ")) 
 	adivina_columna = int(raw_input("Adivina columna: "))
 
 	if (adivina_columna == barco_col) and (adivina_fila== barco_fila):
-		print "Felicitaciones!! HUNDICES MI BARCO :C"
+		print "Felicitaciones!! HUNDISTE MI BARCO :C"
 		break
-	elif adivina_fila not in range(5) or adivina_columna not in range(5):
+	#CONDICIONES QUE MARCAN ERROR
+	elif (adivina_fila <= 0 or adivina_fila > 5 or adivina_columna <= 0  or adivina_columna > 5):
 		print "Eso ni siquiera esta en el oceano"
-	elif (tablero[adivina_fila][adivina_columna]=="X"):
+	elif (tablero[adivina_fila-1][adivina_columna-1]=="X"):
 		print "Ya dijiste esas coordenadas"
 	else:
 		print "Intentalo de nuevo, no tocaste mi barco"
-		tablero[adivina_fila][adivina_columna] = "X"
+		tablero[adivina_fila-1][adivina_columna-1] = "X"
 	print_tablero(tablero)
 	if turno == 4:
+		print
+		print "Se acabaron las oportunidades"
 		print "Fin del juego"
 
