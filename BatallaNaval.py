bf5f605eefa9dc6b520c24129fa88ab0d714c5b6
OmarJVP <omi.jvp@gmail.com>
2016-01-24 18:14:08 -0600

    AGREGAR FUNCIONES PARA DEFINIR POSICION DEL BARCO ALEATORIA

--- BatallaNaval.py
+++ BatallaNaval.py
@@ -1,9 +1,20 @@
-def print_tablero(tablero):
-	for i in range(len(tablero)):
-		print "  ".join(tablero[i])
+from random import randint 
 
 tablero = []
-for i in range(0,5):
-    tablero.append(["O", "O", "O", "O", "O"])
+
+#LLENAR EL TABLERO CON "0"
+for x in range(0, 5):
+    tablero.append(["O"] * 5)
+
+#FUNCION PARA IMPRIMIR TABLERO
+def print_tablero(tablero):
+    for fila in tablero:
+        print " ".join(fila)
+
+#FUNCIONES PARA DEFINIR LA POSICION DEL BARCO DE FORMA ALEATORIA
+def fila_aleatoria(tablero):
+    barco_fila = randint(0, len(tablero)-1)
+def columna_aleatoria(tablero):
+    barco_col = randint(0, len(tablero)-1)
 
 print_tablero(tablero)
