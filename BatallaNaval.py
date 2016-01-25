4105525a5786ff836ef8c79793669704b4580572
OmarJVP <omi.jvp@gmail.com>
2016-01-24 19:06:24 -0600

    Se verifica si el usuario hundio o no el barco

--- BatallaNaval.py
+++ BatallaNaval.py
@@ -14,13 +14,20 @@ def print_tablero(tablero):
 #FUNCIONES PARA DEFINIR LA POSICION DEL BARCO DE FORMA ALEATORIA
 def fila_aleatoria(tablero):
     return randint(0, len(tablero)-1)
+
 def columna_aleatoria(tablero):
     return randint(0, len(tablero)-1)
+
+print_tablero(tablero)
 
 barco_fila = fila_aleatoria(tablero)
 barco_col = columna_aleatoria(tablero)
 
+print barco_fila
+print barco_col
+
 adivina_fila = int(raw_input("Adivina fila: "))
 adivina_columna = int(raw_input("Adivina columna: "))
 
-print_tablero(tablero)
+if (adivina_columna == barco_col) and (adivina_fila== barco_fila):
+	print "Felicitaciones!! HUNDICES MI BARCO :C"
