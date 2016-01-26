d524dfa0204163bd5257691e8b0ba397c18ece0d
Omar <omi.jvp@gmail.com>
2016-01-26 12:07:52 -0600

    El usuario ya puede elegir cuantos barcos desea hundir

--- BatallaNaval.py
+++ BatallaNaval.py
@@ -2,9 +2,16 @@ from random import randint
 
 tablero = []
 
-#LLENAR EL TABLERO CON "0"
-for x in range(0, 5):
-    tablero.append(["O"] * 5)
+print "Juguemos batalla naval"
+print "INTENTA HUNDIR MI BARCO"
+print "Solo tendras 4 oportunidades"
+#PREGUNTA AL USUARIO CUANTOS BARCOS QUIERE ENCONTRAR
+barcos = int(raw_input("Cuantos barcos deseas tratar de hundir:  "))
+
+#LLENAR EL TABLERO CON "0" DEL TAMAnO DETERMINADO
+tamano = barcos*4
+for x in range(tamano):
+    tablero.append(["O"] * tamano)
 
 #FUNCION PARA IMPRIMIR TABLERO
 def print_tablero(tablero):
@@ -18,12 +25,13 @@ def fila_aleatoria(tablero):
 def columna_aleatoria(tablero):
     return randint(1, len(tablero))
 
-print "Juguemos batalla naval"
-print "INTENTA HUNDIR MI BARCO"
 print_tablero(tablero)
+barco_fila = []
+barco_col = []
 
-barco_fila = fila_aleatoria(tablero) 
-barco_col = columna_aleatoria(tablero) 
+for i in range(barcos):
+	barco_fila.append(fila_aleatoria(tablero))
+	barco_col.append(columna_aleatoria(tablero))
 
 print barco_fila
 print barco_col
@@ -33,21 +41,21 @@ for turno in range(1,5):
 	print "TURNO: " + str(turno)
 	adivina_fila = int(raw_input("Adivina fila: ")) 
 	adivina_columna = int(raw_input("Adivina columna: "))
-
-	if (adivina_columna == barco_col) and (adivina_fila== barco_fila):
-		print "Felicitaciones!! HUNDISTE MI BARCO :C"
-		break
-	#CONDICIONES QUE MARCAN ERROR
-	elif (adivina_fila <= 0 or adivina_fila > 5 or adivina_columna <= 0  or adivina_columna > 5):
-		print "Eso ni siquiera esta en el oceano"
-	elif (tablero[adivina_fila-1][adivina_columna-1]=="X"):
-		print "Ya dijiste esas coordenadas"
-	else:
-		print "Intentalo de nuevo, no tocaste mi barco"
-		tablero[adivina_fila-1][adivina_columna-1] = "X"
-	print_tablero(tablero)
-	if turno == 4:
-		print
-		print "Se acabaron las oportunidades"
-		print "Fin del juego"
+	for i in range(barcos):
+		if (adivina_columna == barco_col[i]) and (adivina_fila== barco_fila[i]):
+			print "Felicitaciones!! HUNDISTE MI BARCO :C"
+			break
+		#CONDICIONES QUE MARCAN ERROR
+		elif (adivina_fila <= 0 or adivina_fila > tamano or adivina_columna <= 0  or adivina_columna > tamano):
+			print "Eso ni siquiera esta en el oceano"
+		elif (tablero[adivina_fila-1][adivina_columna-1]=="X"):
+			print "Ya dijiste esas coordenadas"
+		else:
+			print "Intentalo de nuevo, no tocaste mi barco"
+			tablero[adivina_fila-1][adivina_columna-1] = "X"
+		print_tablero(tablero)
+		if turno == 4:
+			print
+			print "Se acabaron las oportunidades"
+			print "Fin del juego"
 
