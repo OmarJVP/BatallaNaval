8f39876e3c5c10482e3da47ad70f6bf20f15c7f6
Omar <omi.jvp@gmail.com>
2016-01-26 13:31:26 -0600

    Ya acepta coordenadas para hundir mas de 1 barco, error si se adivina el barco en la posicion 0 de los arreglos

--- BatallaNaval.py
+++ BatallaNaval.py
@@ -37,25 +37,36 @@ print barco_fila
 print barco_col
 
 for turno in range(1,5):
+	if barcos == 0:
+		print
+		print "HAZ GANADO"
+		break
 	print 
-	print "TURNO: " + str(turno)
+	print "TURNO  " + str(turno)
 	adivina_fila = int(raw_input("Adivina fila: ")) 
 	adivina_columna = int(raw_input("Adivina columna: "))
 	for i in range(barcos):
 		if (adivina_columna == barco_col[i]) and (adivina_fila== barco_fila[i]):
+			barcos -= 1
 			print "Felicitaciones!! HUNDISTE MI BARCO :C"
-			break
-		#CONDICIONES QUE MARCAN ERROR
-		elif (adivina_fila <= 0 or adivina_fila > tamano or adivina_columna <= 0  or adivina_columna > tamano):
-			print "Eso ni siquiera esta en el oceano"
-		elif (tablero[adivina_fila-1][adivina_columna-1]=="X"):
-			print "Ya dijiste esas coordenadas"
-		else:
-			print "Intentalo de nuevo, no tocaste mi barco"
+			print "Te quedan %d barcos por hundir" % barcos
 			tablero[adivina_fila-1][adivina_columna-1] = "X"
-		print_tablero(tablero)
+		#CONDICIONES QUE MARCAN ERROR
+		else:
+			if (adivina_fila <= 0 or adivina_fila > tamano or adivina_columna <= 0  or adivina_columna > tamano):
+				print "Eso ni siquiera esta en el oceano"
+			elif (tablero[adivina_fila-1][adivina_columna-1]=="X"):
+				print "Ya dijiste esas coordenadas"
+			else:
+				if i < barcos:
+					print
+				else:
+					print "Intentalo de nuevo, no tocaste mi barco"
+					tablero[adivina_fila-1][adivina_columna-1] = "X"
 		if turno == 4:
 			print
 			print "Se acabaron las oportunidades"
 			print "Fin del juego"
+	print
+	print_tablero(tablero)
 
